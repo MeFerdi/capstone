8551232e9688a0a60120e4817a900f7179033b10
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-08-31 22:42:37 +0300

    Add: config files for the main func handlers

--- config/config.go
+++ config/config.go
@@ -1,8 +1,12 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 var (
@@ -12,6 +16,7 @@ var (
 	dbHost     = os.Getenv("DB_HOST")
 	dbPort     = os.Getenv("DB_PORT")
 )
+var DB *sqlx.DB
 
 func LoadConfig() {
 	if dbUser == "" || dbPassword == "" || dbName == "" || dbHost == "" || dbPort == "" {
@@ -21,4 +26,16 @@ func LoadConfig() {
 
 func DBConfig() string {
 	return "host=" + dbHost + " port=" + dbPort + " user=" + dbUser + " dbname=" + dbName + " password=" + dbPassword + " sslmode=disable"
+}
+func InitDB() *sqlx.DB {
+	connStr := fmt.Sprintf("user=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_USER"), os.Getenv("DB_NAME"))
+
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	DB = db
+	return db
 }
