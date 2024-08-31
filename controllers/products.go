10ddad9eef6288784d54fd001458cab39f041bec
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-08-31 23:36:03 +0300

    Add: removed unneccesary files and dir setup

--- controllers/products.go
+++ controllers/products.go
@@ -18,6 +18,34 @@ func GetProducts(c *gin.Context) {
 	c.JSON(http.StatusOK, products)
 }
 
+func SearchAndFilterProducts(c *gin.Context) {
+	var products []models.Product
+	searchQuery := c.Query("q")
+	minPrice := c.Query("min_price")
+	maxPrice := c.Query("max_price")
+
+	query := "SELECT * FROM products WHERE 1=1"
+
+	if searchQuery != "" {
+		query += " AND (name ILIKE '%' || $1 || '%' OR description ILIKE '%' || $1 || '%')"
+	}
+
+	if minPrice != "" {
+		query += " AND price >= $2"
+	}
+
+	if maxPrice != "" {
+		query += " AND price <= $30"
+	}
+
+	err := config.DB.Select(&products, query, searchQuery, minPrice, maxPrice)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, products)
+}
+
 func CreateProduct(c *gin.Context) {
 	var product models.Product
 	if err := c.ShouldBindJSON(&product); err != nil {
