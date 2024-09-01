e80c174bdba253baf1a858109b30728e77367ae1
MeFerdi <ferdinandodhis254@gmail.com>
2024-09-01 03:32:02 +0300

    Add: configured the database to receive the program handlers: using structs in place of .env file to store the db credentials

--- config/config.go
+++ config/config.go
@@ -3,51 +3,46 @@ package config
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
-// Define environment variables
-var (
-	dbUser     = os.Getenv("DB_USER")
-	dbPassword = os.Getenv("DB_PASSWORD")
-	dbName     = os.Getenv("DB_NAME")
-	dbHost     = os.Getenv("DB_HOST")
-	dbPort     = os.Getenv("DB_PORT")
-)
+// DatabaseConfig holds the database configuration parameters
+type DatabaseConfig struct {
+	User     string
+	Password string
+	Name     string
+	Host     string
+	Port     string
+}
 
 var DB *sqlx.DB
 
-// LoadConfig checks if all necessary environment variables are set
-func LoadConfig() {
-	if dbUser == "" || dbPassword == "" || dbName == "" || dbHost == "" || dbPort == "" {
-		log.Fatalf("Missing environment variables")
+// NewDatabaseConfig creates a new instance of DatabaseConfig with the given values
+func NewDatabaseConfig(user, password, name, host, port string) *DatabaseConfig {
+	return &DatabaseConfig{
+		User:     user,
+		Password: password,
+		Name:     name,
+		Host:     host,
+		Port:     port,
 	}
-	// Print environment variables for debugging purposes
-	log.Printf("DB_USER: %s", dbUser)
-	log.Printf("DB_PASSWORD: %s", dbPassword)
-	log.Printf("DB_NAME: %s", dbName)
-	log.Printf("DB_HOST: %s", dbHost)
-	log.Printf("DB_PORT: %s", dbPort)
 }
 
 // DBConfig returns the connection string for the database
-func DBConfig() string {
+func (config *DatabaseConfig) DBConfig() string {
 	return fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		dbHost, dbPort, dbUser, dbName, dbPassword)
+		config.Host, config.Port, config.User, config.Name, config.Password)
 }
 
 // InitDB initializes and returns the database connection
-func InitDB() *sqlx.DB {
-	connStr := DBConfig()
-
+func InitDB(config *DatabaseConfig) *sqlx.DB {
+	connStr := config.DBConfig()
 	db, err := sqlx.Connect("postgres", connStr)
 	if err != nil {
-		log.Fatal("Failed to connect to the database:", err)
+		log.Fatalln("Failed to connect to the database:", err)
 	}
-
 	DB = db
 	return db
 }
--- main.go
+++ main.go
@@ -3,29 +3,27 @@ package main
 import (
 	"capstone/config"
 	"capstone/routes"
-	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
 
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-	// Load configurations
-	config.LoadConfig()
+	dbConfig := config.NewDatabaseConfig(
+		"postgres",    // User
+		"ferdinand",   // Password
+		"capstone_db", // Database name
+		"localhost",   // Host
+		"5432",        // Port
+	)
 
 	// Initialize database
-	db := config.InitDB()
+	db := config.InitDB(dbConfig)
 	defer db.Close()
 
 	// Set up routes
+	r := gin.Default()
 	routes.SetupRoutes(r)
 
 	r.Run(":8080")
-
 }
